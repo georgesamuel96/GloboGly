9affb779386228942373ed95cee5b0bd5d8aa8fc
George <georgesamuel96>
2020-01-15 12:44:52 +0200

    Updated City Details

--- app/src/main/java/com/example/georgesamuel/globogly/activities/DestinationDetailActivity.kt
+++ app/src/main/java/com/example/georgesamuel/globogly/activities/DestinationDetailActivity.kt
@@ -3,11 +3,16 @@ package com.example.georgesamuel.globogly.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.georgesamuel.globogly.R
 import com.example.georgesamuel.globogly.helpers.SampleData
 import com.example.georgesamuel.globogly.models.Destination
+import com.example.georgesamuel.globogly.services.DestinationService
+import com.example.georgesamuel.globogly.services.ServiceBuilder
 import kotlinx.android.synthetic.main.activity_destiny_detail.*
+import retrofit2.Call
+import retrofit2.Response
 
 
 class DestinationDetailActivity : AppCompatActivity() {
@@ -35,17 +40,34 @@ class DestinationDetailActivity : AppCompatActivity() {
 	}
 
 	private fun loadDetails(id: Int) {
+		val destinationService = ServiceBuilder.buildService(DestinationService::class.java)
+		val requestCall = destinationService.getDestination(id)
+		requestCall.enqueue(object: retrofit2.Callback<Destination>{
+			override fun onFailure(call: Call<Destination>, t: Throwable) {
+				Toast.makeText(this@DestinationDetailActivity, "Error Occurred $t", Toast.LENGTH_LONG).show()
+			}
 
-		// To be replaced by retrofit code
-		val destination = SampleData.getDestinationById(id)
-
-		destination?.let {
-			et_city.setText(destination.city)
-			et_description.setText(destination.description)
-			et_country.setText(destination.country)
-
-			collapsing_toolbar.title = destination.city
-		}
+			override fun onResponse(call: Call<Destination>, response: Response<Destination>) {
+				if(response.isSuccessful){
+					val destination = response.body()
+					destination?.let {
+						et_city.setText(destination.city)
+						et_description.setText(destination.description)
+						et_country.setText(destination.country)
+						collapsing_toolbar.title = destination.city
+					}
+				}
+				else if(response.code() == 401){
+					Toast.makeText(this@DestinationDetailActivity,
+						getString(R.string.session_expired), Toast.LENGTH_LONG).show()
+				}
+				else {
+					Toast.makeText(this@DestinationDetailActivity,
+						getString(R.string.failed_to_retrive) + " Details",
+						Toast.LENGTH_LONG).show()
+				}
+			}
+		})
 	}
 
 	private fun initUpdateButton(id: Int) {
--- app/src/main/java/com/example/georgesamuel/globogly/services/DestinationService.kt
+++ app/src/main/java/com/example/georgesamuel/globogly/services/DestinationService.kt
@@ -3,9 +3,13 @@ package com.example.georgesamuel.globogly.services
 import com.example.georgesamuel.globogly.models.Destination
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DestinationService {
 
     @GET("destination")
     fun getDestinationList() : Call<List<Destination>>
+
+    @GET("destination/{id}")
+    fun getDestination(@Path("id") id: Int): Call<Destination>
 }
