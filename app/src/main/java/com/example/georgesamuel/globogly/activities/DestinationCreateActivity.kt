d3daf43443196d08b4cfeb92b481fbff40e3c7ce
George <georgesamuel96>
2020-01-15 13:58:27 +0200

    Add new item

--- app/src/main/java/com/example/georgesamuel/globogly/activities/DestinationCreateActivity.kt
+++ app/src/main/java/com/example/georgesamuel/globogly/activities/DestinationCreateActivity.kt
@@ -1,11 +1,16 @@
 package com.example.georgesamuel.globogly.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.georgesamuel.globogly.R
 import com.example.georgesamuel.globogly.helpers.SampleData
 import com.example.georgesamuel.globogly.models.Destination
+import com.example.georgesamuel.globogly.services.DestinationService
+import com.example.georgesamuel.globogly.services.ServiceBuilder
 import kotlinx.android.synthetic.main.activity_destiny_create.*
+import retrofit2.Call
+import retrofit2.Response
 
 class DestinationCreateActivity : AppCompatActivity() {
 
@@ -25,9 +30,25 @@ class DestinationCreateActivity : AppCompatActivity() {
 			newDestination.description = et_description.text.toString()
 			newDestination.country = et_country.text.toString()
 
-			// To be replaced by retrofit code
-			SampleData.addDestination(newDestination)
-            finish() // Move back to DestinationListActivity
+			val destinationService = ServiceBuilder.buildService(DestinationService::class.java)
+			val requestCall = destinationService.addDestination(newDestination)
+			requestCall.enqueue(object: retrofit2.Callback<Destination>{
+				override fun onFailure(call: Call<Destination>, t: Throwable) {
+					Toast.makeText(this@DestinationCreateActivity, "Error Occurred $t", Toast.LENGTH_LONG).show()
+				}
+
+				override fun onResponse(call: Call<Destination>, response: Response<Destination>) {
+					if(response.isSuccessful){
+						Toast.makeText(this@DestinationCreateActivity, getString(R.string.successfully_added),
+							Toast.LENGTH_LONG).show()
+						finish()
+					}
+					else {
+						Toast.makeText(this@DestinationCreateActivity, getString(R.string.failed_to_add_item)
+						, Toast.LENGTH_LONG).show()
+					}
+				}
+			})
 		}
 	}
 }
