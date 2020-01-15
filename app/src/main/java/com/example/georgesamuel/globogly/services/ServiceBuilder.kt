ed76b35196ce47eac55e9a7fc3b4ef8af84ce5dd
George <georgesamuel96>
2020-01-15 14:45:05 +0200

    Add headers

--- app/src/main/java/com/example/georgesamuel/globogly/services/ServiceBuilder.kt
+++ app/src/main/java/com/example/georgesamuel/globogly/services/ServiceBuilder.kt
@@ -1,15 +1,33 @@
 package com.example.georgesamuel.globogly.services
 
+import android.os.Build
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 object ServiceBuilder {
 
     private val BASE_URL = "https://cityapi.getsandbox.com/"
     private val logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-    private val okHttp = OkHttpClient.Builder().addInterceptor(logger)
+    /*
+        Add header for each request
+     */
+    private val headerInterceptor = object: Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            var request = chain.request()
+            request = request.newBuilder()
+                .addHeader("x-device-type", Build.DEVICE)
+                .addHeader("Accept-Language", Locale.getDefault().language)
+                .build()
+            val response = chain.proceed(request)
+            return response
+        }
+    }
+    private val okHttp = OkHttpClient.Builder().addInterceptor(headerInterceptor).addInterceptor(logger)
     private val builder =
         Retrofit.Builder().baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
